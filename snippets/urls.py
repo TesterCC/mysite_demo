35e0b063f8667b189b11773cddd2b6aa7fe7395b
MFC <liyanxi07@gmail.com>
2018-05-25 14:50:25 +0800

    add: refactor by CBV APIView

--- snippets/urls.py
+++ snippets/urls.py
@@ -6,11 +6,11 @@ __time__ = '18/5/25 11:01'
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from snippets.views import snippet_list, snippet_detail
+from snippets.views import SnippetList, SnippetDetail
 
 urlpatterns = [
-    url(r'^snippets/$', snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail),    # http://127.0.0.1:8000/snippets/2/
+    url(r'^snippets/$', SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', SnippetDetail.as_view()),    # http://127.0.0.1:8000/snippets/2/
 ]
 
 # display as  http://example.com/api/items/4.json
