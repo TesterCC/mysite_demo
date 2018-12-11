2a979689986b369b7d675f52e8826afaae7a1bdc
yanxi <testerlyx@foxmail.com>
2018-12-11 17:15:46 +0800

    update model but not migrate, create table directly

--- common/models.py
+++ common/models.py
@@ -14,17 +14,41 @@ class SysSendEmail(models.Model):
     template_id = models.IntegerField(u'模版ID', blank=True, null=True)
     send_flag = models.BooleanField(default=0, verbose_name='是否发送', help_text='是否发送')  # 针对非即时发送，判断是否需要发送消息
     status = models.PositiveSmallIntegerField(verbose_name=u'投递状态', default=0, choices=send_status)
-    email = email = models.EmailField(max_length=50, verbose_name=u'邮箱', blank=True, null=True)
+    email = models.EmailField(max_length=50, verbose_name=u'邮箱', blank=True, null=True)
     tag_id = models.IntegerField(u'邮件服务标记', blank=True, null=True)
+
+    def __str__(self):
+        return '%s' % self.id
 
     class Meta:
         db_table = 'sys__common_send_email'
-        verbose_name = u'活动管理'
+        verbose_name = u'发送邮件管理'
         verbose_name_plural = verbose_name
+
+        index_together = (('event_id', 'email'),)
 
 
 class SysSendSms(models.Model):
+    ops_type = ((0, u'默认发送'), (1, u'主办方审核'), (2, u'下单成功'), (3, u'编辑通过会议审核'))
+    send_status = ((0, u'待发送'), (1, u'发送成功'), (2, u'发送失败'))
+
+    event_id = models.IntegerField(u'活动ID', blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, auto_now_add=True, verbose_name='创建时间', help_text='创建时间')
+    update_time = models.DateTimeField(blank=True, auto_now=True, verbose_name='更新时间', help_text='更新时间')
+    is_delete = models.BooleanField(default=0, verbose_name='是否逻辑删除', help_text='是否逻辑删除')
+    operation_type = models.PositiveSmallIntegerField(verbose_name=u'发送操作类型', default=0, choices=ops_type)
+    msg_data = models.TextField(verbose_name='发送信息内容', help_text="JSON格式存储发送信息内容", null=True, blank=True)
+    template_id = models.IntegerField(u'模版ID', blank=True, null=True)
+    send_flag = models.BooleanField(default=0, verbose_name='是否发送', help_text='是否发送')  # 针对非即时发送，判断是否需要发送消息
+    status = models.PositiveSmallIntegerField(verbose_name=u'投递状态', default=0, choices=send_status)
+    tel = models.CharField(verbose_name=u'联系电话', max_length=11, null=True, blank=True)
+
+    def __unicode__(self):
+        return '%s' % self.id
+
     class Meta:
         db_table = 'sys__common_send_sms'
-        verbose_name = u'活动管理'
+        verbose_name = u'发送短信管理'
         verbose_name_plural = verbose_name
+
+        index_together = (('event_id', 'tel'),)
